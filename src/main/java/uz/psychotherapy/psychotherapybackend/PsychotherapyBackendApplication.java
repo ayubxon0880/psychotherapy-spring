91cc5a7a5dc84f8ff39503bd01174ddbcbf0a6c1
Ayubxon Obidov <128991489+ayubxon0880@users.noreply.github.com>
2025-09-08 14:20:00 +0500

    start

---
+++ src/main/java/uz/psychotherapy/psychotherapybackend/PsychotherapyBackendApplication.java
@@ -0,0 +1,13 @@
+package uz.psychotherapy.psychotherapybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychotherapyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychotherapyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/psychotherapy/psychotherapybackend/PsychotherapyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.psychotherapy.psychotherapybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychotherapyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
